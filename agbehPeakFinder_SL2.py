cff164e2cb04f32b28e533ef9a84a9b96ea2d9b7
michael <michael.rogers@saxslab.com>
2015-06-16 20:42:59 -0400

    I was missing some files

---
+++ agbehPeakFinder_SL2.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import cv2
+from ROOT import TH1D, TH2D, TSpectrum, TCanvas,TVector2
+from npRootUtils import *
+# from root_numpy import fill_hist
+from numpy import *
+import sys
+from pilatus_np import JJTiff
+# im0=retrieveImage('SFU/raw/latest_0000150_caz.tiff',makeU8=True,doLog=True)
+# im0=retrieveImage('SFU/raw/latest_0000150_caz.tiff',doLog=True)
+# im0=retrieveImage('SFU/raw/latest_0000141_caz.tiff',doLog=True) # ~46.2
+# im0=retrieveImage('SFU/raw/latest_0000157_caz.tiff',doLog=True)
+
+# im0=retrieveImage('SFU/raw/latest_0000138_caz.tiff',doLog=True) # ~23.3
+# im0=retrieveImage('SFU/raw/latest_0000166_caz.tiff',doLog=True) # ~235.6
+# im0=retrieveImage('SFU/raw/latest_0000155_caz.tiff',doLog=True) # ~151.9
+# rowCenter=350
+# colCenter=200
+# pSize=500
+# peakThresh=0.005
+# firstPeak=100
+# lastPeak=pSize
+# minDiff=10
+def findPeaks(image,center,peakThresh=0.005,verbose=False,doLogIm=True,pSize=360,firstPeak=70,lastPeak=None,smoothingWindow=5,minDiff=10):
+    """
+
+    findPeaks(center,image,rad)
+
+    Take a saxslab tiff of AgBeh, and return peak spacing, and an array of found peak coordinates (radius from center).
+
+    Peaks in the range (rad[0]:rad[1]) are found by taking slices through the given center coordinate 
+    of the tiff at several rotation agles, and searching for peaks within rad[0]:rad[1]. Then each peak in a slice from
+    the pervious step is separately fit to a gaussian curve, and the mean from each fit is added into a histogram.
+
+    Also, the spacing between all the peaks in a slice is added to a histogram of peak spacings. This is all
+    repeated for each rotation.
+
+    At the end, we have a histogram of peak locations and a histogram of peak spacings. The peak spacing
+    histogram is fit to a gaussian, and the mean, sigma, and error are returned.
+
+    Further, the histogram of peak locations is treated similarly to a slice in the first step, and it's peaks
+    are again fit to gaussians, and the mean, sigma, and error of each are stored in a list of tuples in the output.
+
+     input
+     center:       tuple or list -> (rowCenter,colCenter), where rowCenter is the coord in pix of the center row.
+                                                   colCenter is defined the same way, but for center col.
+     image:        A path to a saxslab tiff, or an np array of a saxslab tiff.
+     rad:          tuple or list -> (minRadius, maxRadius) in which to search for peaks.
+     
+     output
+     tuple:        (peakSpacing, peakSpacingSigma, peakSpacingErr, peaksList)
+                   peakSpacing, peakSpacingSigma, peakSpacingErr are from fitting a histogram of found peak spacings
+                   to a gaussian. 
+                   peaksList is a list of tuples (peak,sig,errPeak,errSig), as radius from center. The peaks are
+                   located again by doing gaussian fits to the peaks in the histogram built from the location of every 
+                   peak found during the slicing and fitting process.
+
+     Returns None on failure.
+    """
+    try:
+
+        # determine if image is an image or a path:
+        if type(image)==str:
+            image=retrieveImage(image,doLog=doLogIm)
+
+        im0=image
+
+        if not lastPeak:
+            lastPeak=int(pSize)
+        else:
+            lastPeak=int(lastPeak)
+        rowCenter=int(center[0])# 350
+        colCenter=int(center[1])# 200
+        peakThresh=float(peakThresh)
+        pSize=int(pSize)
+        rSize=500
+        firstPeak=int(firstPeak)
+        smoothingWindow=int(smoothingWindow)
+        minDiff=int(minDiff)
+        yM,xM=im0.shape
+        peaksHist= TH1D('peaksHist','peaks',rSize*10,0,rSize)
+        dPeaksHist=TH1D('dPeaksHist','dPeaks',rSize,0,rSize)
+        rowHist=TH1D('rowHist','row',rSize,0,rSize)
+            # TH2D(const char* name, const char* title, Int_t nbinsx, Double_t xlow, Double_t xup, Int_t nbinsy, Double_t ylow, Double_t yup)
+        # imPolarHist=TH2D('imPolar','im0 Polar',877*4,0,877,720,0,2*pi)
+        # imPolar=zeros((pSize+1,pSize+1),dtype=uint8)
+        imPolar=zeros((pSize+1,rSize+1))
+        yM,xM=im0.shape
+        vP=TVector2()
+        
+        for x in range(xM):
+            for y in range(yM):
+                vP.SetX(x-colCenter)
+                vP.SetY(y-rowCenter)
+                p=vP.Phi()*pSize/(2*pi)
+                r=vP.Mod()
+                # p=1327./(2*pi)*p
+                # imPolarHist.Fill(r,p,im0[y,x])
+                # print p,r
+                imPolar[round(p),round(r)]=im0[y,x]
+                
+
+        # imPolarHist.Draw()
+        blur = cv2.GaussianBlur(imPolar,(5,5),0)
+
+        sRow=TSpectrum()
+
+
+        for rIdx in range(blur.shape[0]):
+            # row=array(imPolar[rIdx,:],dtype=double).copy()
+            row=blur[rIdx,:]
+            # row=imPolar[0,:]
+            # row[0:row.argmax()]=row.max()
+            sRow.SmoothMarkov(row,len(row),smoothingWindow)
+            setBinsToAr1D(rowHist,row)
+            nFoundRow=sRow.Search(rowHist,1,'goff',peakThresh)
+            xsRow=sRow.GetPositionX()
+            axRow=rwBuf2Array(xsRow,nFoundRow)
+            # axRow[0]=0.0
+            axRow=array([x for x in axRow if x>=firstPeak and x<=lastPeak])
+            fitsRow=fitGausPeaks(rowHist,axRow)
+            
+            arFitsRow=array([x[0] for x in fitsRow])
+            # print 'arFitsRow ',arFitsRow
+            # # arFitsRow[0]=0
+            # # if len(arFitsRow)>1:
+
+            # if arFitsRow[0]<firstPeak:
+            #     arFitsRow=arFitsRow[1:]
+            #     print 'arFitsRow now: ',arFitsRow
+            arDiff=diff(arFitsRow)
+            # print 'before: ',arDiff
+            arDiff=array([x for x in arDiff if x>=minDiff])
+            # print 'after: ',arDiff
+            fill_hist(peaksHist, arFitsRow)
+            fill_hist(dPeaksHist,arDiff)
+
+
+        # tc=TCanvas()
+        # tc.Divide(1,2)
+        # tc.cd(1)
+        # dPeaksHist.Draw()
+        dPmaxBin=dPeaksHist.GetMaximumBin()
+        dPmax=dPeaksHist.GetBinCenter(dPmaxBin)
+        gf=dPeaksHist.Fit('gaus','QSNO','goff',dPmax-5,dPmax+5)
+        dMean=gf.Value(1)
+        dMeanEr=gf.Error(1)
+        dSig=gf.Value(2)
+        dSigEr=gf.Error(2)
+        # tc.cd(2)
+        # this gets the peaks array out at the end
+        peaksHist.Smooth()
+        nFound = sRow.Search(peaksHist,3.5,'goff',0.1)
+        # peaksHist.Draw()
+        xsPeaks=sRow.GetPositionX()
+        aPeaks=rwBuf2Array(xsPeaks,nFound)
+        aPeaks=aPeaks[aPeaks>=firstPeak]
+        aPeaks=aPeaks[aPeaks <= lastPeak]
+        # print aPeaks
+        fitsPeaks=fitGausPeaks(peaksHist,aPeaks,width=10)
+
+        # print fitsPeaks
+        if len(aPeaks)==1:
+            # (mean,sigma,errMean,errSig)
+            nFound = sRow.Search(peaksHist,3.5,'goff',0.1)
+            xsPeaks=sRow.GetPositionX()
+            aPeaks=rwBuf2Array(xsPeaks,nFound)
+            fitsPeaks=fitGausPeaks(peaksHist,aPeaks,width=10)
+            dMean=fitsPeaks[0][0]
+            dMeanEr=fitsPeaks[0][2]
+            dSig=fitsPeaks[0][1]
+            dSigEr=fitsPeaks[0][3]
+        if verbose:
+            print '\nMean Spacing : ',dMean,' +/- ',dMeanEr,'\nSigma        : ',dSig, '+/- ',dSigEr
+                
+        return (dMean, dSig, dMeanEr, dSigEr,fitsPeaks,imPolar,im0)
+    except Exception, e:
+        print e
+        return None  
+
+def retrieveImage(filePath,clearVoids=False,makeU8=False,doLog=False):
+    # return a np array (2dim) of a saxslab tiff file
+
+    try:
+        
+        jjIm=JJTiff(filePath,bars = 0.0, nega = 0.0)
+        image=rot90(fliplr(jjIm.arr))
+        # print 'image: ', type(image),' of: ',image.dtype, ' of shape: ', image.shape
+    except TypeError: # TIFF.open returns a TypeError when it can't read a file
+        print 'Could not open',filePath
+        return
+    # -1, -2, are 255, 254 once we make them uint8, so let's just make the < 0 pix be 0.
+    if clearVoids:
+        image[image<0]=0
+    if doLog:
+        image[image<1]=1
+        image=log(image)
+    if makeU8:
+        image[image<0]=0 # this is needed for uint8
+        imMax=amax(image)
+        im8=array(image/float(imMax)*255,dtype='uint8')
+        return im8
+    else:
+        return image
+def main(argv=sys.argv):
+    im=argv[1]
+    center=(argv[2],argv[3])
+    # doLog=int(argv[4])
+    p=findPeaks(im,center,verbose=True)
+    
+    # p=findPeaks(im,center,verbose=True,rad=(0,110))
+    print
+    print (p,im0,imPolar)
+
+if __name__ == '__main__':
+    main()
---
+++ testRingFinder.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import cv2
+import sys
+import os
+from agbehPeakFinder_SL2 import *
+from numpy import *
+
+def main(argv=sys.argv):
+
+    cv2.namedWindow('Pol')
+    cv2.namedWindow('Log')
+
+    fl=os.listdir(sys.argv[1])
+    tiffDir=sys.argv[1]+'/'
+    fileList=[tiffDir+x for x in fl if x.split('.')[-1]=='tiff']
+    center=(argv[2],argv[3])
+    # set file to first
+    fileIdx=0
+    fn=fileList[fileIdx]
+    y=int(center[0])
+    x=int(center[1])
+    # page through files and fit/draw
+    # hus=[]
+    while 1:
+        # hus.append(main(fn))
+        pX=findPeaks(fn,center)
+        if pX:
+            p=pX[0:5]
+            im0=colorize(pX[6] )
+            imPolar=colorize(pX[5])
+            print fn
+            print p
+            cv2.rectangle(im0,(x,y),(x,y),(0,0,255),-1)
+            circs=[c[0] for c in p[4]]
+            for c in circs:
+                cv2.circle(im0,(x,y),int(c),(0,0,255),1)
+            cv2.imshow('Pol', imPolar)
+            cv2.imshow('Log', im0)
+        else:
+            print 'Nothing found for ',fn
+        fileIdx=doKeyStroke(fileList,fileIdx)
+        if fileIdx<0:
+            break
+
+        fn=fileList[fileIdx]
+        
+    # tschuess
+    # for hu in hus:
+    #     for idx in range(7):
+    #         print '%i: %5.3e' %(idx+1,hu[idx]),
+    #     print
+    cv2.destroyAllWindows()
+def doKeyStroke(fileList,fileIdx):
+    # deal with inconsistencies across implementations
+    if sys.platform=='darwin':
+        nextFileKey=3 # -> 
+        prevFileKey=2 # <-
+    else:
+        nextFileKey=83 #110 # n
+        prevFileKey=81 #112 # p
+
+    # get a keystroke
+    ch = cv2.waitKey(0)
+    # turns out waitkey returns all kinds of unpredictable junk in the upper bits, so we strip it out
+    ch = ch & 255 if ch + 1 else -1;
+    if ch == 27: # pressed esc
+        return -1
+    
+    if ch == nextFileKey:
+        if fileIdx<len(fileList)-1:
+            fileIdx+=1
+    
+    if ch == prevFileKey:
+        if fileIdx>0:
+            fileIdx-=1
+    # print 'ch:',ch
+    return fileIdx
+def colorize(inPic):
+    # print 'inPic.shape',inPic.shape,'\tlen:',len(inPic.shape)
+    if len(inPic.shape)==3:
+        outPic=inPic.copy()
+        inPicGray=cv2.cvtColor(inPic, cv2.COLOR_RGB2GRAY)
+    else:
+
+        inPicGray=inPic.copy() 
+        inPicGray[inPicGray<0]=0 # this is needed for uint8
+        imMax=amax(inPicGray)
+        im8=array(inPicGray/float(imMax)*255,dtype='uint8')
+        outPic=cv2.cvtColor(im8, cv2.COLOR_GRAY2RGB)
+    # print 'inPicGray.shape',inPicGray.shape,'\outPic:',outPic.shape
+    
+    outPic[:,:,0]=inPicGray
+    outPic[:,:,1]=255
+    inPicGC=inPicGray.copy()
+    inPicGC[inPicGray<1]=0
+    inPicGC[inPicGray>=1]=255
+    outPic[:,:,2]=inPicGC.copy() 
+    outPic=cv2.cvtColor(outPic,cv2.COLOR_HSV2RGB)
+    return outPic
+
+if __name__ == '__main__':
+    main()
+
+    
+cv2.namedWindow('rot')
